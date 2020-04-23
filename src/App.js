be4834442163701b0f440960fe3a6e7e1997fe3a
Amaleshwar Rao Palthi <you@example.com>
2020-04-23 17:34:57 +0530

    addedCarouselComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {React.version}
-      </header>
+      <Carousel  autoPlay interval={3000} transitionTime={1000} infiniteLoop={true}>
+    <div>
+      <img className="size"alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+      <p className="legend">Legend 1</p>
+    </div>
+    <div>
+      <img className="size"alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
+      <p className="legend">Legend 2</p>
+    </div>
+    <div>
+      <img className="size" alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
+      <p className="legend">Legend 3</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" />
+      <p className="legend">Legend 4</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" />
+      <p className="legend">Legend 5</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" />
+      <p className="legend">Legend 6</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" />
+      <p className="legend">Legend 7</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" />
+      <p className="legend">Legend 8</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" />
+      <p className="legend">Legend 9</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" />
+      <p className="legend">Legend 10</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" />
+      <p className="legend">Legend 11</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" />
+      <p className="legend">Legend 12</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" />
+      <p className="legend">Legend 13</p>
+    </div>
+    <div>
+      <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" />
+      <p className="legend">Legend 14</p>
+    </div>
+  </Carousel>
     </div>
   );
 }
